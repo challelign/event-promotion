abad6b22f7c78acede834649cf67ff8a93c3bb3e
Challelign Tilahun <chalie2123@gmail.com>
2024-01-08 16:01:33 +0300

    remove font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,14 @@
 import type { Metadata } from "next";
-import { Poppins } from "next/font/google";
+// import { Poppins } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 
 import "./globals.css";
 
-const poppins = Poppins({
-	subsets: ["latin"],
-	weight: ["400", "500", "600", "700"],
-	variable: "--font-poppins",
-});
+// const poppins = Poppins({
+// 	subsets: ["latin"],
+// 	weight: ["400", "500", "600", "700"],
+// 	variable: "--font-poppins",
+// });
 
 export const metadata: Metadata = {
 	title: "Evently",
@@ -26,7 +26,8 @@ export default function RootLayout({
 	return (
 		<ClerkProvider>
 			<html lang="en">
-				<body className={poppins.variable}>{children}</body>
+				{/* <body className={poppins.variable}>{children}</body> */}
+				<body>{children}</body>
 			</html>
 		</ClerkProvider>
 	);
