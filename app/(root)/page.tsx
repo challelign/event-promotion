bd30757c74cd39e72af519130d1e5044878758fd
Challelign Tilahun <chalie2123@gmail.com>
2024-01-09 15:47:44 +0300

    change font size and make fit to the device

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -26,11 +26,11 @@ export default async function Home({ searchParams }: SearchParamProps) {
 				{/* <p className=" bg-red-800 max-w-6xl lg:mx-auto p-5 md:px-10 xl:px-0 w-full   text-white">
 					chalie
 				</p> */}
-				<div className="wrapper grid grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-0">
-					<div className="flex flex-col justify-center gap-8">
-						<h1 className="h1-bold">
+				<div className="wrapper grid grid-cols-1 gap-3 md:grid-cols-2 2xl:gap-0">
+					<div className="flex flex-col justify-center gap-4">
+						<h3 className="h3-bold">
 							Host, Connect, Celebrate: Your Events, Our Platform!
-						</h1>
+						</h3>
 						<p className="p-regular-20 md:p-regular-24">
 							Book and learn helpful tips from 3,168+ mentors in world-class
 							companies with our global community.
@@ -51,11 +51,11 @@ export default async function Home({ searchParams }: SearchParamProps) {
 			</section>
 			<section
 				id="events"
-				className="wrapper my-8 flex flex-col gap-8 md:gap-12"
+				className="wrapper my-4 flex flex-col gap-4 md:gap-6"
 			>
-				<h2 className="h2-bold ">
+				<h4 className="h3-bold ">
 					Trusted by <br /> Thousands of Events
-				</h2>
+				</h4>
 				<div className="flex w-full flex-col gap-5 md:flex-row ">
 					<Search /> <CategoryFilter />
 				</div>
